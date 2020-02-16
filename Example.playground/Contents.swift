339f0531b562c89fc456b57804b9dad6aa2f7967
Chris <cjnevin@gmail.com>
2020-02-16 08:56:54 +0800

    Added layers to playground.

--- Example.playground/Contents.swift
+++ Example.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// MARK: - Common
+
 protocol AnyCounterView {
     var presenter: AnyCounterPresenter? { get set }
 
@@ -21,100 +23,106 @@ protocol AnyStorage {
     func setValue<Value>(_ value: Value, forKey key: String)
 }
 
-struct GetCountUseCase {
-    let storage: AnyStorage
-
-    func get() -> Int {
-        storage.getValue(forKey: "count") ?? 0
-    }
-}
-
-struct IncrementUseCase {
-    let storage: AnyStorage
-
-    func increment() -> Int {
-        let get = GetCountUseCase(storage: storage)
-        let count = get.get()
-        storage.setValue(count + 1, forKey: "count")
-        return get.get()
-    }
-}
-
 protocol AnyCounterCoordinator {
     var storage: AnyStorage { get }
 
     func makeCounterView() -> AnyCounterView
 }
 
-class CounterPresenter: AnyCounterPresenter {
-    private var view: AnyCounterView?
-    private let getCountUseCase: GetCountUseCase
-    private let incrementUseCase: IncrementUseCase
+// MARK: - Domain
 
-    init(coordinator: AnyCounterCoordinator) {
-        getCountUseCase = GetCountUseCase(storage: coordinator.storage)
-        incrementUseCase = IncrementUseCase(storage: coordinator.storage)
+enum Domain {
+    private struct GetCountUseCase {
+        let storage: AnyStorage
+
+        func get() -> Int {
+            storage.getValue(forKey: "count") ?? 0
+        }
     }
+    private struct IncrementUseCase {
+        let storage: AnyStorage
 
-    func attach<View: AnyCounterView>(view: View) {
-        self.view = view
-        view.setCount(getCountUseCase.get())
+        func increment() -> Int {
+            let get = GetCountUseCase(storage: storage)
+            let count = get.get()
+            storage.setValue(count + 1, forKey: "count")
+            return get.get()
+        }
     }
+    class CounterPresenter: AnyCounterPresenter {
+        private var view: AnyCounterView?
+        private let getCountUseCase: GetCountUseCase
+        private let incrementUseCase: IncrementUseCase
 
-    func detach() {
-        view?.setCount(0)
-        view = nil
-    }
+        init(coordinator: AnyCounterCoordinator) {
+            getCountUseCase = GetCountUseCase(storage: coordinator.storage)
+            incrementUseCase = IncrementUseCase(storage: coordinator.storage)
+        }
 
-    func increment() {
-        view?.setCount(incrementUseCase.increment())
-    }
-}
+        func attach<View: AnyCounterView>(view: View) {
+            self.view = view
+            view.setCount(getCountUseCase.get())
+        }
 
-class RealView: AnyCounterView {
-    var presenter: AnyCounterPresenter?
+        func detach() {
+            view?.setCount(0)
+            view = nil
+        }
 
-    func appeared() {
-        presenter?.attach(view: self)
-    }
-
-    func disappeared() {
-        presenter?.detach()
-    }
-
-    func tappedIncrement() {
-        presenter?.increment()
-    }
-
-    func setCount(_ value: Int) {
-        print(value)
-    }
-}
-
-class RealStorage: AnyStorage {
-    typealias Key = String
-    private var storage: [Key: Any] = [:]
-
-    func getValue<Value>(forKey key: Key) -> Value? {
-        storage[key] as? Value
-    }
-
-    func setValue<Value>(_ value: Value, forKey key: Key) {
-        storage[key] = value
+        func increment() {
+            view?.setCount(incrementUseCase.increment())
+        }
     }
 }
 
-class RealCoordinator: AnyCounterCoordinator {
-    var storage: AnyStorage = RealStorage()
+// MARK: - App
 
-    func makeCounterView() -> AnyCounterView {
-        let view = RealView()
-        view.presenter = CounterPresenter(coordinator: self)
-        return view
+enum App {
+    class CounterView: AnyCounterView {
+        var presenter: AnyCounterPresenter?
+
+        func appeared() {
+            presenter?.attach(view: self)
+        }
+
+        func disappeared() {
+            presenter?.detach()
+        }
+
+        func tappedIncrement() {
+            presenter?.increment()
+        }
+
+        func setCount(_ value: Int) {
+            print(value)
+        }
+    }
+
+    class CounterStorage: AnyStorage {
+        typealias Key = String
+        private var storage: [Key: Any] = [:]
+
+        func getValue<Value>(forKey key: Key) -> Value? {
+            storage[key] as? Value
+        }
+
+        func setValue<Value>(_ value: Value, forKey key: Key) {
+            storage[key] = value
+        }
+    }
+
+    class CounterCoordinator: AnyCounterCoordinator {
+        var storage: AnyStorage = CounterStorage()
+
+        func makeCounterView() -> AnyCounterView {
+            let view = CounterView()
+            view.presenter = Domain.CounterPresenter(coordinator: self)
+            return view
+        }
     }
 }
 
-let coordinator = RealCoordinator()
+let coordinator = App.CounterCoordinator()
 let view = coordinator.makeCounterView()
 view.appeared()
 view.tappedIncrement()
